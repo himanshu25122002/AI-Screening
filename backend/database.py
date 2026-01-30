94688cbe24335b3690b52702106e818d8ec3c676
himanshu kumar bhagat <himanshubhagat2512@gmail.com>
2026-01-30 14:50:30 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from supabase import create_client, Client
-from config import config
+from backend.config import config
 
 def get_supabase_client() -> Client:
     return create_client(config.SUPABASE_URL, config.SUPABASE_SERVICE_KEY)
 
 supabase = get_supabase_client()
+
