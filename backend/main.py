395dbd566a3f7ba885fc793516fa4758c649b6ae
himanshu kumar bhagat <himanshubhagat2512@gmail.com>
2026-03-16 22:53:14 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,12 +26,7 @@ from backend.services.interview_schedule import router as interview_schedule_rou
 app = FastAPI(title="AI Candidate Screening API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://ai-screening-six.vercel.app",  
-        "http://localhost:3000",                
-        "http://localhost:8501",
-        "https://www.futureadymedia.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
